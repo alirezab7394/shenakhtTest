f753f64f1b5fb2515a6ea83623afa756e4f10a35
alirezab7394 <alireza7394@gmail.com>
2021-08-04 01:15:33 +0430

    add provider and layout to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Layout from "./components/Layout";
+import TasKProvider from "./components/Providers/TasksProvider";
+import TaskContainer from "./components/TaskContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <TasKProvider>
+          <TaskContainer />
+        </TasKProvider>
+      </Layout>
     </div>
   );
 }
