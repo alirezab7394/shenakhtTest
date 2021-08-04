0ec627790deb6ed96efab4b3b782816ebd22a4e3
alirezab7394 <alireza7394@gmail.com>
2021-08-04 19:40:04 +0430

    add links to link dot

--- src/components/Widgets/LinkDot/index.tsx
+++ src/components/Widgets/LinkDot/index.tsx
@@ -1,12 +1,19 @@
 import React, { memo } from "react";
+import { Link } from "react-router-dom";
 import "./style.scss";
 
-interface LinkDotProps {}
-function LinkDot() {
+interface LinkDotProps {
+  hasChildren: boolean;
+  isOpen: boolean;
+  id: number;
+}
+function LinkDot({ hasChildren, isOpen, id }: LinkDotProps) {
   return (
-    <div className="LinkDot">
-      <span className="LinkDot--main">&#9679;</span>
-    </div>
+    <Link to={`/${id}`}>
+      <div className={`LinkDot ${hasChildren && !isOpen && "light"}`}>
+        <span className="LinkDot--main">&#x25CF;</span>
+      </div>
+    </Link>
   );
 }
 export default memo(LinkDot);
