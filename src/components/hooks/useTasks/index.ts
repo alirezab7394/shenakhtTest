90bfda326075e9914bdb6322f0b5c0b59281f145
alirezab7394 <alireza7394@gmail.com>
2021-08-04 19:35:12 +0430

    add new functions

--- src/components/hooks/useTasks/index.ts
+++ src/components/hooks/useTasks/index.ts
@@ -1,9 +1,9 @@
-import { useContext } from "react";
 import { TaskContext } from "src/components/Providers/TasksProvider";
+import { useContext } from "react";
 
 const useTasks = () => {
-  const { tasks, editTask } = useContext(TaskContext);
-  return { tasks, editTask };
+  const { tasks, editTask, addNewTask ,removeTask,changeParent} = useContext(TaskContext);
+  return { tasks, editTask, addNewTask ,removeTask,changeParent};
 };
 
 export default useTasks;
