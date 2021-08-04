2bf5ac7bcbb74e0dbd93ac3340f50acb181c1246
alirezab7394 <alireza7394@gmail.com>
2021-08-04 19:35:56 +0430

    feature header

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import Button from "src/components/Widgets/Button";
+import {ReactComponent as Back} from "src/assets/svg/arrow_back_ios.svg"
+import {ReactComponent as Home} from "src/assets/svg/home.svg"
+import "./style.scss"
 
 export default function Header() {
-  return <div />;
+  const history = useHistory();
+  return (
+    <div className="Header">
+      <Button shape="iconButton" onClick={() => history?.goBack()}><Back/></Button>
+      <Button shape="iconButton" style={{marginLeft:"1rem"}} onClick={() => history?.push("/")}><Home/></Button>
+    </div>
+  );
 }
